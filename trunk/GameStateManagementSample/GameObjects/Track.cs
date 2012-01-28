65d7d063e55c10202077ecfcb963f827b5a8fe59
tjohns20@gmail.com <tjohns20@gmail.com@c0739855-8096-2525-18e2-764296e8324b>
2012-01-28 17:20:44 +0000

    integrated box2d and some loading stuff

---
+++ trunk/GameStateManagementSample/GameObjects/Track.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using System.Xml;
+using Box2D.XNA;
+
+namespace GameStateManagement.GameObjects
+{
+    public class Track
+    {
+        public void Load()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("level.oel");
+
+
+            foreach (XmlElement e in doc["level"]["Track"].GetElementsByTagName("TrackEdge") )
+            {
+                //TODO: add segment to the collision segments
+            }
+        }
+
+    }
+}
