ccbfda3e8dc9f9a65f894ec845b4ff8a75574732
jacobkwitkoski@gmail.com <jacobkwitkoski@gmail.com@c0739855-8096-2525-18e2-764296e8324b>
2012-01-28 07:33:50 +0000

    Cleaned up player class.

--- GameStateManagementSample/GameObjects/Player.cs
+++ GameStateManagementSample/GameObjects/Player.cs
@@ -11,48 +11,41 @@ namespace GameStateManagement.GameObjects
 {
     class Player
     {
-        protected Vector2 position2;
+        protected Vector2 position;
 
         public Vector2 Position2
         {
-            get { return position2; }
+            get { return position; }
             set
             {
-                position2.X = position3.X = value.X;
-                position2.Y = position3.Y = value.Y;
-                position3.Z = 0;
+                position.X = value.X;
+                position.Y = value.Y;
             }
         }
 
-        protected Vector3 position3;
-
         public Vector3 Position3
         {
-            get { return position3; }
+            get { return new Vector3(position, 0); }
             set
             {
-                position3.X = position2.X = value.X;
-                position3.Y = position2.Y = value.Y;
-                position3.Z = value.X;
+                position.X = value.X;
+                position.Y = value.Y;
             }
         }
 
         public Player()
         {
-            Position2 = Vector2.Zero;
-            Position3 = Vector3.Zero;
+            position = Vector2.Zero;
         }
 
         public Player(float x, float y)
         {
-            Position2 = new Vector2(x, y);
-            Position3 = new Vector3(x, y, 0);
+            position = new Vector2(x, y);
         }
 
         public Player(Vector2 position)
         {
-            Position2 = position;
-            Position3 = new Vector3(position, 0);
+            this.position = position;
         }
     }
 }
