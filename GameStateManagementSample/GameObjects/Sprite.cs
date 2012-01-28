1769edbe3db6d15442ada91daa080523f59057f5
jacobkwitkoski@gmail.com <jacobkwitkoski@gmail.com@c0739855-8096-2525-18e2-764296e8324b>
2012-01-28 17:17:54 +0000

    Added base sprite class, made player inherit from it.

---
+++ GameStateManagementSample/GameObjects/Sprite.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameStateManagement.GameObjects
+{
+    class Sprite : DrawableGameComponent
+    {
+        protected ScreenManager screenManager;
+
+        public Texture2D texture;
+
+        protected Vector2 position;
+
+        public Vector2 Position2
+        {
+            get { return position; }
+            set
+            {
+                position.X = value.X;
+                position.Y = value.Y;
+            }
+        }
+
+        public Vector3 Position3
+        {
+            get { return new Vector3(position, 0); }
+            set
+            {
+                position.X = value.X;
+                position.Y = value.Y;
+            }
+        }
+
+        public Sprite(ScreenManager screenManager)
+            : base(screenManager.Game)
+        {
+        }
+    }
+}
