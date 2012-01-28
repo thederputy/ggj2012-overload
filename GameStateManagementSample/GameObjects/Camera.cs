ae48372ab050689d2060bde7436deeb47e1aa89b
altimeter@gmail.com <altimeter@gmail.com@c0739855-8096-2525-18e2-764296e8324b>
2012-01-28 08:08:48 +0000

    Added camera class to hold view and projection matrices.
    Added camera member to player class and altered constructors to create one.
    
    Altered player update to update the view matrix for the camera.
    
    Altered gameplayscreen to use camera from Player
    
    TODO: fix regression: need to pass matrices to spritebatch begin to fix camera tracking

---
+++ GameStateManagementSample/GameObjects/Camera.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+
+namespace GameStateManagement.GameObjects
+{
+    class Camera
+    {
+        public Matrix View;
+        public Matrix Projection;
+
+        private  Vector3 cameraZPlane = new Vector3(0, 0, 10.0f);
+
+        public Camera(Vector3 playerPosition)
+        {
+            View = Matrix.CreateLookAt(playerPosition + cameraZPlane, playerPosition, Vector3.Up);
+            Projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, 2.0f / 3.0f, 1.0f, 10000f);
+        }
+
+        public void Update(Vector3 playerPosition)
+        {
+            View = Matrix.CreateLookAt(playerPosition + cameraZPlane, playerPosition, Vector3.Up);
+        }
+    }
+}
