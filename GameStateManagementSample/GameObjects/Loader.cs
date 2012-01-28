6c4fe46d68bf1d83c9c72ab16a2e22832e43706b
tjohns20@gmail.com <tjohns20@gmail.com@c0739855-8096-2525-18e2-764296e8324b>
2012-01-28 17:20:44 +0000

    integrated box2d and some loading stuff

---
+++ GameStateManagementSample/GameObjects/Loader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using Microsoft.Xna.Framework;
+using Box2D.XNA;
+
+namespace GameStateManagement
+{
+    class Track
+    {
+        public XmlDocument Xml { get; private set; }
+        Body Body { get; set; }
+
+
+        public void Load(string level)
+        {
+            Xml = new XmlDocument();
+            Xml.Load(level);
+
+            World world = new World(Vector2.Zero, false);
+
+            BodyDef def = new BodyDef();
+            def.type = BodyType.Static;
+            Body body = world.CreateBody(def);
+            body.SetUserData(this);
+            EdgeShape shape = new EdgeShape();
+            shape.Set(Vector2.Zero, Vector2.One);
+            body.CreateFixture(shape, 1.0f);
+        }
+    }
+}
