56a8047e711f2d4653fafdb4a80a8739723deaad
altimeter@gmail.com <altimeter@gmail.com@c0739855-8096-2525-18e2-764296e8324b>
2012-01-28 17:24:11 +0000

    Updated PowerSource.cs to use new Sprite class

---
+++ GameStateManagementSample/GameObjects/PowerSource.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameStateManagement.GameObjects
+{
+    class PowerSource : Sprite
+    {
+        private int charge;
+        private float timeToLive; //seconds
+        
+        public PowerSource(ScreenManager screenManager) : base(screenManager)
+        {
+        }
+
+        public override void Initialize()
+        {
+            charge = 5;
+            timeToLive = 5;
+            position = new Vector2(288, 464);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            texture = Game.Content.Load<Texture2D>("Sprites\\powersource");
+            base.LoadContent();
+        }
+    }
+}
