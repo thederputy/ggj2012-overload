213264e6bacec9456e53b544bbf525e65dc980a9
jacobkwitkoski@gmail.com <jacobkwitkoski@gmail.com@c0739855-8096-2525-18e2-764296e8324b>
2012-01-28 17:17:54 +0000

    Added base sprite class, made player inherit from it.

--- trunk/GameStateManagementSample/GameObjects/Player.cs
+++ trunk/GameStateManagementSample/GameObjects/Player.cs
@@ -9,36 +9,13 @@ using Microsoft.Xna.Framework.Input;
 
 namespace GameStateManagement.GameObjects
 {
-    class Player : GameComponent
+    class Player : Sprite
     {
         #region Fields
         public Camera camera;
 
-        protected Vector2 position;
-
-
         protected int X_LIMIT = 100;
         protected int Y_LIMIT = 100;
-
-        public Vector2 Position2
-        {
-            get { return position; }
-            set
-            {
-                position.X = value.X;
-                position.Y = value.Y;
-            }
-        }
-
-        public Vector3 Position3
-        {
-            get { return new Vector3(position, 0); }
-            set
-            {
-                position.X = value.X;
-                position.Y = value.Y;
-            }
-        }
 
         protected Vector2 velocity;
         public Vector2 Velocity
@@ -49,26 +26,37 @@ namespace GameStateManagement.GameObjects
 
         #endregion
 
+        #region Initialization
+
         public Player(ScreenManager screenManager)
-            :base (screenManager.Game)
+            :base(screenManager)
         {
             position = Vector2.Zero;
             camera = new Camera(screenManager.GraphicsDevice.Viewport, Position3);
         }
 
         public Player(ScreenManager screenManager, float x, float y)
-            :base (screenManager.Game)
+            :base(screenManager)
         {
             position = new Vector2(x, y);
             camera = new Camera(screenManager.GraphicsDevice.Viewport, Position3);
         }
 
         public Player(ScreenManager screenManager, Vector2 position)
-            :base (screenManager.Game)
+            :base(screenManager)
         {
             this.position = position;
             camera = new Camera(screenManager.GraphicsDevice.Viewport, Position3);
         }
+
+        protected override void LoadContent()
+        {
+            Game.Content.Load<Texture2D>("Sprites/car");
+
+            base.LoadContent();
+        }
+
+        #endregion
 
         public override void Update(GameTime gameTime)
         {
