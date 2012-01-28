6f1765e9fcd18e7bd71c2de151222fea1e6976f8
jacobkwitkoski@gmail.com <jacobkwitkoski@gmail.com@c0739855-8096-2525-18e2-764296e8324b>
2012-01-28 06:54:57 +0000

    Got rid of random jitter from player 2.

---
+++ GameStateManagementSample/GameObjects/Player.cs
@@ -0,0 +1,16 @@
+﻿#region Using Statements
+using System;
+using System.Threading;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+#endregion
+
+namespace GameStateManagement.GameObjects
+{
+    class Player
+    {
+        public Vector2 position;
+    }
+}
