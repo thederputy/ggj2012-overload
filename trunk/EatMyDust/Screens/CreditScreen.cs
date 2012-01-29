5aeb9aad830df5da56b4a65f3a0dbcb414c56814
altimeter@gmail.com <altimeter@gmail.com@c0739855-8096-2525-18e2-764296e8324b>
2012-01-29 21:08:33 +0000

    Added info for CreditScreen.cs

--- trunk/EatMyDust/Screens/CreditScreen.cs
+++ trunk/EatMyDust/Screens/CreditScreen.cs
@@ -10,10 +10,19 @@ namespace EatMyDust
 {
     class CreditScreen : GameScreen
     {
-        
+        String createdBy;
+        String thanksTo;
         public override void LoadContent()
         {
             inputManager = new InputManager(ScreenManager.Game);
+
+            createdBy = "Tim Hargreaves@Tatham Johnson@James Karg@Jacob Kwitkoski@Shannon Lee@Tom McIntosh@Shane Morin";
+            createdBy = createdBy.Replace("@", System.Environment.NewLine);
+
+            thanksTo = "Nick Waanders@Graham Jans@Jake Birkett@Brian Provinciano for the@Retro City Rampage cars@Shane Neville@Dr. Box & Kim Voll@Starbucks@Vancouver GGJ Community@opengame.org and FreeSound";
+            thanksTo = thanksTo.Replace("@", System.Environment.NewLine);
+
+
             base.LoadContent();
         }
 
@@ -29,10 +38,16 @@ namespace EatMyDust
 
         public override void Draw(Microsoft.Xna.Framework.GameTime gameTime)
         {
-            ScreenManager.GraphicsDevice.Clear(Color.Black);
+            //ScreenManager.GraphicsDevice.Clear(Color.Black);
             SpriteBatch spriteBatch = ScreenManager.SpriteBatch;
             spriteBatch.Begin();
-            spriteBatch.DrawString(ScreenManager.Font, "Credits text", new Vector2(25), Color.White);
+            spriteBatch.DrawString(ScreenManager.Font, "Credits", new Vector2(ScreenManager.GraphicsDevice.Viewport.Width/2 - 30, 30), Color.White);
+            spriteBatch.DrawString(ScreenManager.Font, "Created By", new Vector2(250, 90), Color.White);
+            spriteBatch.DrawString(ScreenManager.Font, createdBy, new Vector2(250, 150), Color.White);
+
+            spriteBatch.DrawString(ScreenManager.Font, "Thanks To", new Vector2(ScreenManager.GraphicsDevice.Viewport.Width/2 + 90, 90), Color.White);
+            spriteBatch.DrawString(ScreenManager.Font, thanksTo, new Vector2(ScreenManager.GraphicsDevice.Viewport.Width/2 + 90, 150), Color.White);
+
             spriteBatch.End();
 
             base.Draw(gameTime);
