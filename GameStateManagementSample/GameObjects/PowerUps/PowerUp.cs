4139ff2d6b79bcc260c95cefc464ade59551dea0
jacobkwitkoski@gmail.com <jacobkwitkoski@gmail.com@c0739855-8096-2525-18e2-764296e8324b>
2012-01-29 00:02:28 +0000

    Added powerUp.

---
+++ GameStateManagementSample/GameObjects/PowerUps/PowerUp.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Box2D.XNA;
+
+namespace GameStateManagement.GameObjects.PowerUps
+{
+    /// <summary>
+    /// This is a game component that implements IUpdateable.
+    /// </summary>
+    public class PowerUp : GameObject
+    {
+        /// <summary>
+        /// The type of powerup it is.
+        /// </summary>
+        public enum Type
+        {
+            PositionSwap,
+            Spawn,
+            SpeedBoost
+        }
+
+        public PowerUp(ScreenManager screenManager, World physicsWorld)
+            : base(screenManager, physicsWorld)
+        {
+            // TODO: Construct any child components here
+        }
+
+        /// <summary>
+        /// Allows the game component to perform any initialization it needs to before starting
+        /// to run.  This is where it can query for any required services and load content.
+        /// </summary>
+        public override void Initialize()
+        {
+            // TODO: Add your initialization code here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// Allows the game component to update itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        public override void Update(GameTime gameTime)
+        {
+            // TODO: Add your update code here
+
+            base.Update(gameTime);
+        }
+    }
+}
