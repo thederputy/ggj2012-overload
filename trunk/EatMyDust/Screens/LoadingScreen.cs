cf3c35e00ddd98a49b2113e0faa3be6c2ed3b164
tjohns20@gmail.com <tjohns20@gmail.com@c0739855-8096-2525-18e2-764296e8324b>
2012-01-29 22:44:41 +0000

    player controls in instructions

--- trunk/EatMyDust/Screens/LoadingScreen.cs
+++ trunk/EatMyDust/Screens/LoadingScreen.cs
@@ -158,7 +158,9 @@ namespace EatMyDust
                 spriteBatch.DrawString(font, "HOW TO PLAY:", textPosition, color);
                 spriteBatch.DrawString(font, "Avoid Obstacles!", textPosition + new Vector2(0, textSize.Y), color);
                 spriteBatch.DrawString(font, "Grab Powerups!", textPosition + new Vector2(0, 2*textSize.Y), color);
-                spriteBatch.DrawString(font, "Use each other's exhaust to get as far as possible!", textPosition + new Vector2(0, 3*textSize.Y), color);
+                spriteBatch.DrawString(font, "Use each other's exhaust to get as far as possible!", textPosition + new Vector2(0, 3 * textSize.Y), color);
+                spriteBatch.DrawString(font, "Player1: W, A, S, D , LeftCtrl", textPosition + new Vector2(0, 4 * textSize.Y), color);
+                spriteBatch.DrawString(font, "Player2: Arrow Keys , RightCtrl", textPosition + new Vector2(0, 5 * textSize.Y), color);
 
                 spriteBatch.DrawString(font, message, new Vector2(0, viewport.Height - textSize.Y), color);
                 spriteBatch.End();
