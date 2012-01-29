266eb1557989c46cf59e33fc626bf57d7636ed79
altimeter@gmail.com <altimeter@gmail.com@c0739855-8096-2525-18e2-764296e8324b>
2012-01-29 20:29:03 +0000

    Actually added Credit screen

---
+++ trunk/EatMyDust/Screens/CreditScreen.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace EatMyDust
+{
+    class CreditScreen : GameScreen
+    {
+        
+        public override void LoadContent()
+        {
+            inputManager = new InputManager(ScreenManager.Game);
+            base.LoadContent();
+        }
+
+        public override void Update(Microsoft.Xna.Framework.GameTime gameTime, bool otherScreenHasFocus, bool coveredByOtherScreen)
+        {
+            inputManager.Update(gameTime);
+            if (inputManager.currentKeyboardState.IsKeyDown(Keys.Escape))
+            {
+                ExitScreen();
+            }
+            base.Update(gameTime, otherScreenHasFocus, coveredByOtherScreen);
+        }
+
+        public override void Draw(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            ScreenManager.GraphicsDevice.Clear(Color.Black);
+            SpriteBatch spriteBatch = ScreenManager.SpriteBatch;
+            spriteBatch.Begin();
+            spriteBatch.DrawString(ScreenManager.Font, "Credits text", new Vector2(25), Color.White);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+
+    }
+}
